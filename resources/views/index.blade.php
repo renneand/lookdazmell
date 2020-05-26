985c90aca3887968a527fbcc10ab4425b08bb310
renneand <rennemelo@hotmail.com>
2020-05-26 20:42:57 -0300

    Criação do projeto

---
+++ resources/views/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.layout')
+
+@section('conteudo')
+
+LookDazMell
+
+@endsection
---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+        <meta name="csrf-token" content="{{csrf_token()}}">
+    </head>
+   <body>
+        <div class="">
+
+        </div>
+
+        @yield('conteudo')
+    </body>
+    <footer>
+        <div class=""></div>
+    </footer>
+</html>
